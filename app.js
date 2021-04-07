377bf54bf7f4fd6915338a0c8523fb9e50be72ff
nmparfait <mbamu.developer@gmail.com>
2021-04-07 11:55:56 -0400

    increase, decrease and reset is working without clicking on the btn

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-function counter(element, value) {
-  this.element = element;
-  this.value = value;
-  this.resetBtn = element.querySelector('.reset');
-  this.decreaseBtn = element.querySelector('.decrease');
-  this.increaseBtn = element.querySelector('.increase');
-  this.valueDOM = element.querySelector('.value');
-  this.valueDOM.textContent = this.value;
-  console.log(this.decreaseBtn);
-  console.log(this.valueDOM);
-}
-
-const firstCounter = new counter(getElement('.first-counter'), 100);
-const secondCounter = new counter(getElement('.second-counter'), 200);
-
+//helper function to get access to the class
 function getElement(selection) {
   const element = document.querySelector(selection);
   if (element) {
@@ -23,3 +9,36 @@ function getElement(selection) {
     );
   }
 }
+
+function counter(element, value) {
+  this.element = element;
+  this.value = value;
+  this.resetBtn = element.querySelector('.reset');
+  this.decreaseBtn = element.querySelector('.decrease');
+  this.increaseBtn = element.querySelector('.increase');
+  this.valueDOM = element.querySelector('.value');
+  this.valueDOM.textContent = this.value;
+  //console.log(this.decreaseBtn);
+  //console.log(this.valueDOM);
+}
+
+//adding more options to our object
+counter.prototype.increase = function () {
+  console.log(this);
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+
+counter.prototype.decrease = function () {
+  console.log(this);
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new counter(getElement('.first-counter'), 100);
+const secondCounter = new counter(getElement('.second-counter'), 200);
